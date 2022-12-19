a386189210e0d86ffc8c43e89173034568894c05
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2022-12-19 14:41:27 +0200

    small fix

--- src/views.py
+++ src/views.py
@@ -15,18 +15,17 @@ async def get_weather_from_url_1(session, url):
         data = await response.text()
         soup = BeautifulSoup(data, 'html.parser')
 
-        # Get temperature
         temp = soup.find('span', class_='CurrentConditions--tempValue--MHmYY').text[0:2]
         temp = f'{temp}°C'
-        # Get wind speed
+
         wind = soup.find('span', class_='Wind--windWrapper--3Ly7c undefined').text
         wind = get_wind_speed_in_ms_normalize(wind)
-        # Get humidity
+
         humidity = soup.findAll('div', class_='WeatherDetailsListItem--wxData--kK35q')[2].text
-        # # Get pressure
+
         pressure = soup.find('span', class_='Pressure--pressureWrapper--3SCLm undefined').text
         pressure = get_pressure_normalize(pressure)
-        # # Get weather description
+
         weather_desc = soup.find('div', class_='CurrentConditions--phraseValue--mZC_p').text
 
         return temp, wind, humidity, pressure, weather_desc
@@ -36,19 +35,19 @@ async def get_weather_from_url_2(session, url):
     async with session.get(url) as response:
         data = await response.text()
         soup = BeautifulSoup(data, 'html.parser')
-        # Get temperature
+
         temp = soup.find('span', class_='unit unit_temperature_c').text
         temp = f'{temp}°C'
-        # Get wind speed
+
         wind = soup.find('div', class_='unit unit_wind_m_s').text
         wind = get_wind_speed_normalize(wind)
-        # Get humidity
+
         humidity = soup.find('div', class_='now-info-item humidity').text
         humidity = get_humidity_normalize(humidity)
-        # # Get pressure
+
         pressure = soup.find('div', class_='unit unit_pressure_mm_hg_atm').text
         pressure = get_pressure_normalize(pressure)
-        # # Get weather description
+
         weather_desc = soup.find('div', class_='now-desc').text
 
         return temp, wind, humidity, pressure, weather_desc
@@ -59,17 +58,16 @@ async def get_weather_from_url_3(session, url):
         data = await response.text()
         soup = BeautifulSoup(data, 'html.parser')
 
-        # Get temperature
         temp = soup.find('a', class_='summaryTemperatureCompact-E1_1 summaryTemperatureHover-E1_1').text
-        # Get wind speed
+
         wind = soup.find('div', id='CurrentDetailLineWindValue').text
         wind = get_wind_speed_in_ms_normalize(wind)
-        # Get humidity
+
         humidity = soup.find('div', id='CurrentDetailLineHumidityValue').text
-        # # Get pressure
+
         pressure = soup.find('div', id='CurrentDetailLinePressureValue').text
         pressure = get_pressure_normalize(pressure)
-        # # Get weather description
+
         weather_desc = soup.find('div', class_='summaryCaptionCompact-E1_1').text
 
         return temp, wind, humidity, pressure, weather_desc
